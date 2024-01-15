e8423add7901a06a11503dc1a75ec22dc123f321
s8st <153998744+s8st@users.noreply.github.com>
2024-01-15 09:02:42 +0900

    [level Lv.0] Title: 양꼬치, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer;
+        if(n<10)
+        { answer = (n*12000)+(k*2000);}
+        else
+        { answer = (n*12000)+(k*2000)-((n/10)*2000);}
+        return answer;
+    }
+}
